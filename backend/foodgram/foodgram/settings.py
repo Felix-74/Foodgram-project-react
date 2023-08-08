470d2fd7d9bfea6cdb9f3330be06a65b4f1cbec7
Igor <igor-teplov@yandex.ru>
2023-08-08 13:29:50 +0300

    update for prod

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJ_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 AUTH_USER_MODEL = 'users.User'
